35b878fe1522032904ba50beb5a65490656ba258
jefti <jeftimeira@gmail.com>
2023-05-14 12:22:47 -0300

    retirada a funcionalidade de apresentar a primeira ltra em maiusculo

--- src/componentes/Resposta.js
+++ src/componentes/Resposta.js
@@ -3,6 +3,6 @@ export default function Resposta(props){
     const teste = LetrasJogadas.includes(letra);
     const texto = teste? letra :'_';
     return (
-        (numero == 0)? <div className="DisplayLetra">{texto.toUpperCase()}</div> : <div className="DisplayLetra">{texto}</div>
+        (numero == 0)? <div className="DisplayLetra">{texto}</div> : <div className="DisplayLetra">{texto}</div>
     )
 }
